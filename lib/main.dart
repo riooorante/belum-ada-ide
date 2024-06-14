1a8f92d65376cd0e0ac1d8bd110e8b3ed343b303
riooorante <rantetadungrio@gmail.com>
2024-06-14 22:59:33 +0800

    feat: add project

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_application_1/splash.dart';
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/home': (context) => const HomePage(),
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
